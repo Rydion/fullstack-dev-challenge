5f25a46f1e6bbd22cabc2df8f2fec133d77669e1
Adrian Hintze <hintze.adrian@gmail.com>
2018-07-21 16:29:43 +0100

    Refactoring: Quick autoformat and added missing semicolons where necessary.

--- client/src/components/CurrencyInput.js
+++ client/src/components/CurrencyInput.js
@@ -4,41 +4,42 @@ import './CurrencyInput.css'
 
 export default class CurrencyInput extends Component {
 	constructor(props) {
-		super(props)
+        super(props);
 
-		this.state = {
-			hasFocus: false,
-			value: props.defaultValue
-		}
+        this.state = {
+            hasFocus: false,
+            value: props.defaultValue
+        };
 	}
 
 	handleChange(e) {
-		const value = e.target.value
-		this.setState({value})
+        const value = e.target.value;
+        this.setState({ value });
 	}
 
 	handleFocus(e) {
-		this.setState({
-			hasFocus: true
-		})
+        this.setState({
+            hasFocus: true
+        });
 	}
 
 	render() {
-		const { defaultValue } = this.props
-		const { value } = this.state
+        const { defaultValue } = this.props;
+        const { value } = this.state;
 
-		return (
-			<div className={`currency-input ${defaultValue !== undefined ? 'default-value' : ''}`}>
-				<span>£</span>
-				<input type="text"
-					value={value}
-					onChange={this.handleChange.bind(this)}
-					onFocus={this.handleFocus.bind(this)}/>
-			</div>
-		)
+        return (
+            <div className={`currency-input ${defaultValue !== undefined ? 'default-value' : ''}`}>
+                <span>£</span>
+                <input type="text"
+                    value={value}
+                    onChange={this.handleChange.bind(this)}
+                    onFocus={this.handleFocus.bind(this)}
+                />
+            </div>
+        );
 	}
 }
 
 CurrencyInput.propTypes = {
-	defaultValue: PropTypes.number
-}
+    defaultValue: PropTypes.number
+};
--- client/src/components/SliderInput.js
+++ client/src/components/SliderInput.js
@@ -3,37 +3,37 @@ import PropTypes from 'prop-types'
 import './SliderInput.css'
 
 export default class SliderInput extends Component {
-
 	constructor(props) {
-		super(props)
+        super(props);
 
-		this.state = {
-			value: props.defaultValue
-		}
+        this.state = {
+            value: props.defaultValue
+        };
 	}
 
 	handleChange(e) {
-		const value = e.target.value
-		this.setState({value})
+        const value = e.target.value;
+        this.setState({ value });
 	}
 
 	render() {
-		const { value } = this.state
+        const { value } = this.state;
 
-		return (
-			<div className="fmz-slider">
-				<p>{value}%</p>
-				<input type="range"
-					value={value}
-					min={0}
-					max={10}
-					step={0.25}
-					onChange={this.handleChange.bind(this)}/>
-			</div>
-		)
+        return (
+            <div className="fmz-slider">
+                <p>{value}%</p>
+                <input type="range"
+                    value={value}
+                    min={0}
+                    max={10}
+                    step={0.25}
+                    onChange={this.handleChange.bind(this)}
+                />
+            </div>
+        );
 	}
 }
 
 SliderInput.propTypes = {
-	defaultValue: PropTypes.number
-}
+    defaultValue: PropTypes.number
+};
